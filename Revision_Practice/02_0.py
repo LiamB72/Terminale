6ca9980ad97e32d64f1ebfe1230ea7317aec363f
Liam B <112952569+LiamB72@users.noreply.github.com>
2023-09-15 15:50:29 +0200

    Delete Revision_Practice directory

--- Revision_Practice/02_0.py
+++
@@ -1,4 +0,0 @@
-for i in range(0,6):
-    for j in range(0, 6):
-        print(f"{i*j:3}", end="")
-    print()
--- Revision_Practice/03.py
+++
@@ -1,5 +0,0 @@
-def convert(euro):
-    dollars = euro * 1.12
-    print(f"\n{euro}€ = {dollars}$\n")
-    
-convert(69)
