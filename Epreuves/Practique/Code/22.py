b38505aadacd5cd0167c0155d67c3d7e42cf18f0
Liam B <112952569+LiamB72@users.noreply.github.com>
2023-11-14 10:13:02 +0100

    Update 22.py

--- Epreuves/Practique/Code/22.py
+++ Epreuves/Practique/Code/22.py
@@ -7,13 +7,13 @@ def dichotomie(tab, x):
     debut = 0
     fin = len(tab) - 1
     while debut <= fin:
-        m = int(fin/2)
+        m = (debut + fin)//2
         if x == tab[m]:
             return True
         if x > tab[m]:
             debut = m + 1
         else:
-            fin = m - 1
+            fin -= 1
     return False
 
 
