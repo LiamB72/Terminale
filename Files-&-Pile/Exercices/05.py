526e2844dca781ce84e3eea3146b0b615bdb02de
Liam B <112952569+LiamB72@users.noreply.github.com>
2023-11-28 11:04:01 +0100

    Update 05.py

--- Files-&-Pile/Exercices/05.py
+++ Files-&-Pile/Exercices/05.py
@@ -3,8 +3,27 @@ from fileSansPrio import File
 
 def croisement(f1, f2):
     f3 = File()
-    # Acompléter (6 conditions if dans une boucle while)
+    while not f1.estVide() and not f2.estVide():
+        pop1 = f1.defiler()
 
+        if pop1 == 1:
+            f3.enfiler(pop1)
+
+        elif pop1 == 0:
+            pop2 = f2.defiler()
+
+            if pop2 == 2:
+                f3.enfiler(pop2)
+
+            elif pop2 == 0:
+                f3.enfiler(0)
+
+    if f1.estVide():
+        for i in range(len(f2.file)):
+            f3.enfiler(f2.defiler())
+    elif f2.estVide():
+        for i in range(len(f1.file)):
+            f3.enfiler(f1.defiler())
     return f3
 
 
