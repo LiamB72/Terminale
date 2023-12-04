a7ca82d96d85521843674befb153c1e609dbb558
Liam B <112952569+LiamB72@users.noreply.github.com>
2023-12-04 21:57:36 +0100

    work has been done

--- Epreuves/Subjects/Subject0/01.py
+++ Epreuves/Subjects/Subject0/01.py
@@ -7,8 +7,25 @@ grid = [[0,  0,  1,  0],
 robotSaved = []
 nb_rows = 5
 nb_cols = 4
+nbRobot = {1:(0,3),2:(2,0),3:(4,1),4:(2,3)}
 
-def go_west():
+def bouger(direction:str):
+    """Moves with the act of a letter.
+
+    direction (str): N: North, S: South, O:Ouest, E:Est
+    """
+    if direction == "N":
+        pass #go_North()
+    elif direction == "S":
+        pass #go_South()
+    elif direction == "O":
+        go_West()
+    elif direction == "E":
+        pass #go_East()
+    else:
+        print("Wrong letter!")
+
+def go_West():
     for row in range(nb_rows):
         for col in range(nb_cols):
             contentCase = grid[row][col]
@@ -24,13 +41,11 @@ def go_west():
                         grid[row][col - 1] = contentCase
 
 def robotStatus(n:int) -> int | tuple:
-    for row in range(nb_rows):
-        for col in range(nb_cols):
+    if grid.index(n) == nbRobot[n]:
+        return nbRobot[n]
 
-            if n == grid[row][col]:
-                return row, col # Returns a tuple of the Robot's Coordinates in the grid.
+    elif n in robotSaved:
+        return 1
 
-    if n in robotSaved:
-        return 1 # If robot ain't in the grid but in the saved list, return 1
-
-    return -1 # Returns whenever neither of both previous cases aren't true.
+    return -1
+    
