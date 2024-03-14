550b3e20bf711a76265cc82d1dac571a547b13af
Liam B <112952569+LiamB72@users.noreply.github.com>
2024-03-14 17:58:30 +0100

    Create 01-1.py

---
+++ 09-BNS2024/01-1.py
@@ -0,0 +1,14 @@
+a = {'F':['B','G'], 'B':['A','D'], 'A':['',''], 'D':['C','E'], \
+'C':['',''], 'E':['',''], 'G':['','I'], 'I':['','H'], \
+'H':['','']}
+
+def taille(abr, lettre):
+    if lettre == "":
+        return 0
+    return 1 + taille(abr, abr[lettre][0]) + taille(abr, abr[lettre][1])
+    
+    
+    
+print(taille(a, 'F'))
+print(taille(a, 'B'))
+print(taille(a, 'I'))
